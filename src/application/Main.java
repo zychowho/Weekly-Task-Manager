27080e9ec8f0e6cf5b74ca131bf5cb466e8c82f8
jona <jonalynbua@gmail.com>
2023-11-19 04:52:26 +0800

    Added initial project

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Parent root  = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+			Scene scene = new Scene(root);
+			
+			primaryStage.setTitle("Login!");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
